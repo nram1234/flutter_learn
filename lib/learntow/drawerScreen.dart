4a5b60ac4eb9c429f06122b79f3ca32b88860be6
nram1234 <redanaser0@gmail.com>
2020-05-08 12:31:29 +0200

    l

--- lib/learntow/drawerScreen.dart
+++ lib/learntow/drawerScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'configuration.dart';
 
-
 class DrawerScreen extends StatefulWidget {
   @override
   _DrawerScreenState createState() => _DrawerScreenState();
@@ -13,7 +12,16 @@ class _DrawerScreenState extends State<DrawerScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: primaryGreen,
-
+      child: Stack(
+        children: <Widget>[
+         Align( alignment: Alignment.topLeft,child: Text('9999999999'),), Align(
+            child: ListView(
+              children: <Widget>[Text('8888'),Text('8888'),Text('8888'),Text('8888')],
+            ),
+            alignment: Alignment.centerLeft,
+          )
+       ,   Align( alignment: Alignment.bottomLeft,child: Text('9999999999'),), ],
+      ),
     );
   }
 }
